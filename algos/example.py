b195e325536f61bd19b213651d164d7301801268
rauladriaensen98 <35867802+rauladriaensen98@users.noreply.github.com>
2021-02-19 23:10:53 +0100

    testing files setup

---
+++ algos/example.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 23:00:27 2021
+
+@author: acse-ra2617
+"""
+
+"Testing about"
+
+__all__ = ['Summation']
+
+
+def Summation(a, b):
+    """
+    given two values, 'a' and 'b' the sum of these values are returned
+    parameters
+    ----------
+    a : float or int
+        any value
+    b : float or int
+        any value
+    examples
+    --------
+    >>> a = 2
+    >>> b = 3
+    >>> c = sum(a,b)
+    >>> c
+    5
+    """
+    return a+b
