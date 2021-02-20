8bb86782b210db2143468f26349037e1887c2646
rauladriaensen98 <35867802+rauladriaensen98@users.noreply.github.com>
2021-02-20 01:28:35 +0100

    experimenting calling data

---
+++ algos/connect.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 01:19:25 2021
+
+@author: raula
+"""
+
+# https://python-binance.readthedocs.io/en/latest/
+
+API_KEY = '3JhGiDQJDXEYfkDYVJYrMJNscerTnCrY3Sx4PpfmyuFzvxW1vHzm4yuCyF9ilfEn'
+API_SECRET = 'wYGB950qDpyLcL3jRdB2IDZaCfwRxyuf7PwFs9wuZ7PPVA6yVBbsnbp4vZrTcTaR'
